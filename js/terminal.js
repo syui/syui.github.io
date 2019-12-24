24fc0fd3c44f7c776754f18310da4a62949ddb9b
syui <syui@users.noreply.github.com>
2019-12-24 16:54:29 +0000

    Automated deployment: Tue Dec 24 16:54:29 UTC 2019 9245833e29b7ac91a1baaa3bd5720ba5b1d0e869

--- js/terminal.js
+++ js/terminal.js
@@ -1,4 +1,5 @@
 $(function() {
+
     var pacman_install_message_post = "";
     var prompt = "[[b;#d33682;]user]@[[b;#6c71c4;]syui.cf] ~$ ";
     var days_left = Math.round((new Date('2016 01 01') - new Date()) / (1000 * 60 * 60 * 24));
@@ -98,11 +99,30 @@ $(function() {
         this.pause();
         this.currentTime = 0.0;
     }
+     function progress(percent, width) {
+        var size = Math.round(width*percent/100);
+        var left = '', taken = '', i;
+        for (i=size; i--;) {
+            taken += '=';
+        }
+        if (taken.length > 0) {
+            taken = taken.replace(/=$/, '>');
+        }
+        for (i=width-size; i--;) {
+            left += ' ';
+        }
+        return '[' + taken + left + '] ' + percent + '%';
+    }
+    var animation = false;
+    var timer;
+    var prompt;
+    var string;
     function interpreter(input, term) {
         var command, inputs;
 	var myAudio = new Audio();
         inputs = input.split(/ +/)
         command = inputs[0];
+
         if (input === "ls /json") {
             term.echo("/json/link.json");
         } else if (inputs[0] === 'ls /json/link.json' || inputs[0] === '/gpg/pgp_keys.asc.txt') {
@@ -133,6 +153,9 @@ $(function() {
             bash(inputs, term);
         } else if (/whoami/.test(input)) {
             term.echo("user");
+        } else if (/nyancat/.test(input)) {
+            term.echo("/nyancat");
+	    window.open('/nyancat/');
         } else if (/uname/.test(input)) {
             term.echo("Linux");
         } else if (command.length === 0) {} else if (/ls/.test(input)) {
@@ -160,6 +183,86 @@ $(function() {
         }
     };
 
+    //var url = 'https://terminal.jcubic.pl/ansi/read.php'; // CORS enabled
+    //function get(url) {
+    //    return fetch(url).then(res => res.text()).then(text=>text.trim());
+    //};
+    //var files = (await get(url + '?action=ls')).split('\n');
+    //function get_ansi(name) {
+    //    return fetch(url + '?raw=1&file=' + name).
+    //        then(res => res.arrayBuffer()).then(a => new Uint8Array(a));
+    //};
+    function echo_ansi(buff) {
+        var meta = AnsiLove.sauceBytes(buff);
+        var cols = 80;
+        var str = iconv.decode(buff, 'CP437');
+        // remove code that move cursor after new line - hope this works
+        str = str.replace(/\r?\n?\x1b\[A\x1b\[[0-9]+C/g, '');
+        // main code - this will use unix_formatting file to format ANSI artwork
+        str = $.terminal.apply_formatters(str, {});
+        if (meta) {
+            if (str.match(/\x1a/)) { // 26 - end of file in DOS
+                str = str.replace(/\x1a.*/, '');
+            }
+            console.log(meta);
+            cols = meta.tInfo1;
+            var lines = $.terminal.split_equal(str, cols);
+            // one flush to output whole image to the screen
+            lines.map(line => term.echo(line, {flush: false}));
+            term.flush();
+        } else {
+            // should we wrap to 80 cols if there is no meta SAUCE data?
+            term.echo(str);
+        }
+        term.resume();
+    };
+	//var term = $('body').terminal({
+        //help: function() {
+        //    this.echo([
+        //        'use [[;#444;]ls] to display list of files and [[;#444;]cat]',
+        //        ' to display the artwork, you can also [[u;;]drag and drop] the file.',
+        //        '\nOnly Code Page 437 encoding files will work (classic demo s',
+        //        'cene files).\nType [[;#444;]credits] to show credits for ANSI',
+        //        ' Art works. Use tab so you don\'t need to type whole filename.',
+        //        '\nType [[;#444;]clear] to clear terminal.'
+        //    ].join(''), {keepWords: true});
+        //},
+        //cat: function(name) {
+        //    this.pause();
+        //    get_ansi(name).then(echo_ansi);
+        //},
+        //ls: function() {
+        //    console.log(files);
+        //    this.echo(files);
+        //},
+        //credits: function() {
+        //    this.echo('ANSI artworks taken from [[!;;;;https://fuel.wtf/packs/fuel27/]fuel magazine]')
+        //}
+    	//}, {
+        //completion: function(string, callback) {
+        //    if (this.get_command().match(/^cat /)) {
+        //        callback(files);
+        //    } else {
+        //        callback(['ls', 'file', 'credits', 'help']);
+        //    }
+        //},
+        //outputLimit: 250,
+        //greetings: 'ANSI ART Terminal viewer\nType [[;#444;]help] for more info\n',
+        //name: 'ansi'
+    	//}).drop(function(array_buff) { // drop plugin
+    	//    echo_ansi(new Uint8Array(array_buff));
+    	//    return term.read('press enter to continue ');
+    	//}, {
+    	//    type: 'arraybuffer',
+    	//    error: function(e) {
+    	//        term.exception(e);
+    	//    },
+    	//    complete: function() {
+    	//        term.echo('all files rendered');
+    	//    }
+    	//});
+
+
     $('#terminal').terminal(interpreter, {
         prompt: prompt,
         name: 'test',
@@ -174,14 +277,14 @@ $(function() {
 		if (t.match(/cat/)) {
             		callback(["/json/link.json","/json/keybase.json","/json/file.json","/gpg/pgp_keys.asc.txt"]);
 		} else if (t.match(/help/)){
-            		callback(["help","ls","cat"]);
+            		callback(["help","ls","cat","nyancat"]);
 		} else {
-            		callback(["help","ls","cat"]);
+            		callback(["help","ls","cat","nyancat"]);
 		}
         },
         tabcompletion: true
+
     });
 });
-
 
 
