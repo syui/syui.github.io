18aece9748c10cbf6c4dd4ebe826c0a035dfd37b
syui <syui@syui.cf>
2021-07-03 16:14:09 +0900

    fix

--- content/imgall.md
+++ content/imgall.md
@@ -5,7 +5,5 @@ page_image : "https://syui.cf/favicon.png"
 description: "illust"
 ---
 
-<div id=app></div>
-<script src=/imgauto/chunk-vendors.js></script>
-<script src=/imgauto/app.js></script>
+
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/zsh
-
-d=${0:h:a}
-cp -r $d/src-imgauto/dist/*.js $d/static/imgauto/
-cp -r $d/src-imgauto/dist/*.map $d/static/imgauto/
-cp -r $d/src-imgauto/dist/*.css $d/static/imgauto/
+export IMG_YUI=`ls ./static/img/yui_*.png|wc -l|tr -d ' '`
+export IMG_YUIC=`ls ./static/img/c_*.png|wc -l|tr -d ' '`
+export IMG_YUIA=`ls ./static/img/ai_*.png|wc -l|tr -d ' '`
+export IMG_YUIO=`ls ./static/img/octo_*.png|wc -l|tr -d ' '`
+export IMG_YUII=`ls ./static/img/item_*.png|wc -l|tr -d ' '`
+export IMG_YUIF=`ls ./static/img/f_*.png|wc -l|tr -d ' '`
