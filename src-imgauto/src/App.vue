5aea7c3b9b57d38469d137fd521032ee862285da
syui <syui@syui.cf>
2021-07-03 12:00:51 +0900

    up imgauto

--- src-imgauto/src/App.vue
+++ src-imgauto/src/App.vue
@@ -10,16 +10,61 @@
 			</a>
 		</p>
 	</div>
+	<div class="img-chara">
+		<p>
+			<a v-for="(n,index) of cid" :key="n" :href="'/img/c_'+ ( '00' + index ).slice( -2 ) +'.png'">
+				<img :src="'/img/c_'+ ( '00' + index ).slice( -2 ) +'.png'" />
+			</a>
+		</p>
+	</div>
+	<div class="img-ai">
+		<p>
+			<a v-for="(n,index) of aid" :key="n" :href="'/img/ai_'+ ( '00' + index ).slice( -2 ) +'.png'">
+				<img :src="'/img/ai_'+ ( '00' + index ).slice( -2 ) +'.png'" />
+			</a>
+		</p>
+	</div>
+	<div class="img-octo">
+		<p>
+			<a v-for="(n,index) of oid" :key="n" :href="'/img/octo_'+ ( '00' + index ).slice( -2 ) +'.png'">
+				<img :src="'/img/octo_'+ ( '00' + index ).slice( -2 ) +'.png'" />
+			</a>
+		</p>
+	</div>
+	<div class="img-item">
+		<p>
+			<a v-for="(n,index) of iid" :key="n" :href="'/img/item_'+ ( '00' + index ).slice( -2 ) +'.png'">
+				<img :src="'/img/item_'+ ( '00' + index ).slice( -2 ) +'.png'" />
+			</a>
+		</p>
+	</div>
+	<div class="img-field">
+		<p>
+			<a v-for="(n,index) of fid" :key="n" :href="'/img/f_'+ ( '00' + index ).slice( -2 ) +'.png'">
+				<img :src="'/img/f_'+ ( '00' + index ).slice( -2 ) +'.png'" />
+			</a>
+		</p>
+	</div>
 </div>
 </template>
 
 <script>
 var e = Number(process.env.VUE_APP_IMGNUMBER - 2);
+var a = Number(process.env.VUE_APP_IMGNUMBERA);
+var c = Number(process.env.VUE_APP_IMGNUMBERC);
+var o = Number(process.env.VUE_APP_IMGNUMBERO);
+var i = Number(process.env.VUE_APP_IMGNUMBERI);
+var f = Number(process.env.VUE_APP_IMGNUMBERF);
 export default {
 	data () {
 		return {
 			id: e,
 			lid: e,
+			aid: a,
+			cid: c,
+			oid: o,
+			iid: i,
+			fid: f,
 			lurl: '/img/yui_' + e + '.png'
 		}
 	}
