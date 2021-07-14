af784437e22775d257c3edd5e687eee3fa00321f
syui <syui@syui.cf>
2021-07-14 13:03:24 +0900

    add manga en

---
+++ src-manga-en/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <hooper :settings="hooperSettings">
+    <slide v-for="(n,index) of products" :key="n">
+					<img :src="'/manga/en/'+ (index) +'.png'" />
+     <div class="page_n">{{ n }}</div>
+    </slide>
+    <hooper-navigation slot="hooper-addons"></hooper-navigation>
+    <hooper-pagination slot="hooper-addons"></hooper-pagination>
+    <hooper-progress slot="hooper-addons"></hooper-progress>
+  </hooper>
+</template>
+
+<script>
+import {
+  Hooper,
+  Slide,
+  Progress as HooperProgress,
+  Pagination as HooperPagination,
+  Navigation as HooperNavigation
+} from 'hooper';
+
+import 'hooper/dist/hooper.css';
+
+export default {
+  name: 'App',
+  components: {
+    Hooper,
+    Slide,
+    HooperProgress,
+    HooperPagination,
+    HooperNavigation
+  },
+  data() {
+    return {
+      products: [...Array(Number(process.env.VUE_APP_PAGE)).keys()],
+      hooperSettings: {
+        itemsToShow: 1,
+        centerMode: true
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.hooper {
+	height: 100%;
+}
+button.hooper-indicator {
+	background-color: #000;
+}
+img {
+	width:100%;
+}
+.page_n {
+	text-align: center;
+	height: 50px;
+}
+.hooper-pagination {
+  position: fixed;
+}
+.hooper-next, .hooper-prev {
+    background-color: #b8cfe03d;
+}
+</style>
