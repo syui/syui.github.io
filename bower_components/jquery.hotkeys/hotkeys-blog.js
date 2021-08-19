aeff42cf8132cf9036536024ee098d34852cd70d
syui <syui@users.noreply.github.com>
2021-08-19 04:32:44 +0000

    deploy: 0d861747621e3201df13f7876e7e1fee5898873b

--- bower_components/jquery.hotkeys/hotkeys-blog.js
+++ bower_components/jquery.hotkeys/hotkeys-blog.js
@@ -1,6 +1,29 @@
 $(function(){
+
+$("a").closest("li").find("a.lmove")[0].focus();
 	$(document).bind('keydown','ctrl+return',
 		function(){
 			window.location.href = '/blog';
-		});
+		})
+
+
+	$(document).bind('keydown', 'Shift+j',
+		function(){      
+			if (document.activeElement !== document.body){
+				$(".lmove:focus").closest("li").next().find("a.lmove").focus();
+			} else {
+   	$('#fmove').focus();
+			}
+		}
+	)
+	$(document).bind('keydown', 'Shift+k',
+		function(){      
+			if (document.activeElement !== document.body){
+				$(".lmove:focus").closest("li").prev().find("a.lmove").focus();
+			} else {
+   	$('#fmove').focus();
+			}
+		}
+	)
+
 });
