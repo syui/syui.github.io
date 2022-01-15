ad5cfc46f2e1315dc3831bc527a637392fcba470
syui <syui@syui.cf>
2022-01-15 14:13:43 +0900

    fix

--- src-tarot/convert.zsh
+++ src-tarot/convert.zsh
@@ -6,7 +6,7 @@ dd=${0:a:h:h}
 n=`cat $d/static/json/tarot.json|jq "length"`
 n=`expr $n - 1`
 bg=$dd/static/img/tarot_bg.png
-
+font="/System/Library/AssetsV2/com_apple_MobileAsset_Font6/5ef536f846908ec81f4b37caef397b3cb050b64e.asset/AssetData/ToppanBunkyuGothicPr6N.ttc"
 for ((i=0;i<=$n;i++))
 do
 	p=`cat $d/static/json/tarot.json|jq -r ".[$i].p"`
@@ -17,9 +17,9 @@ do
 	echo "$s -> $o"
 	composite -gravity north  -geometry +0+160 -compose over $s $bg $o
 	if [ `echo $h|wc -m` -eq 2 ];then
-	mogrify -font "/System/Library/AssetsV2/com_apple_MobileAsset_Font6/5ef536f846908ec81f4b37caef397b3cb050b64e.asset/AssetData/ToppanBunkyuGothicPr6N.ttc" -fill white -pointsize 200 -annotate +930+2570 "$h" $o
+	mogrify -font "$font" -fill white -pointsize 200 -annotate +930+2570 "$h" $o
 else
-	mogrify -font "/System/Library/AssetsV2/com_apple_MobileAsset_Font6/5ef536f846908ec81f4b37caef397b3cb050b64e.asset/AssetData/ToppanBunkyuGothicPr6N.ttc" -fill white -pointsize 200 -annotate +830+2570 "$h" $o
+	mogrify -font "$font" -fill white -pointsize 200 -annotate +830+2570 "$h" $o
 	fi
 	squoosh-cli --webp '{"quality":100}' -d $dd/content/ai/tarot/ --resize '{width:400,height:550}' $o
 done
