2cd32f7ded1faa3d5203c2942afdd05c535e1f46
syui <syui@syui.ai>
2023-03-30 14:37:54 +0900

    fix

--- src-tarot/convert.zsh
+++ src-tarot/convert.zsh
@@ -13,7 +13,10 @@ dir=$dd/content/ai/tarot
 static=$dd/static/img
 json=$dd/static/json/tarot.json
 jsons=$d/static/json/tarot.json
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+nvm use 17
 unset good
 unset goodh
 
